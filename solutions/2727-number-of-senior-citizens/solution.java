6bbbf222b2c7f076ef2aa907c02dd813bf33f470
Agasthya R Kumar <147073909+agasthyarkumar@users.noreply.github.com>
2024-08-01 10:38:21 +0000

    [LeetCode] Runtime - 1 ms (69.55%), Memory - 42.7 MB (100.00%)

---
+++ solutions/2727-number-of-senior-citizens/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countSeniors(String[] details) {
+      int count=0;
+      for(String detail:details){
+        int a=detail.charAt(11)-'0';
+        int b=detail.charAt(12)-'0';
+        int ageval=a*10+b;
+        if(ageval>60) {count++;}
+      }
+      return count;  
+    }
+}
