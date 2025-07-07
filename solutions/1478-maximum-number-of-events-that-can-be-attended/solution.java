e5a95166a5499f504bc192a06192203e7d9fe080
Agasthya R Kumar <147073909+agasthyarkumar@users.noreply.github.com>
2025-07-07 14:07:45 +0000

    [LeetCode] Runtime - 69 ms (65.52%), Memory - 75.6 MB (100.00%)

---
+++ solutions/1478-maximum-number-of-events-that-can-be-attended/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+  public int maxEvents(int[][] events) {
+    Arrays.sort(events, Comparator.comparingInt(e -> e[0]));
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    int ans = 0, day = 0, i = 0, n = events.length;
+
+    while (i < n || !minHeap.isEmpty()) {
+      if (minHeap.isEmpty()) {
+        day = events[i][0];
+      }
+      while (i < n && events[i][0] <= day) {
+        minHeap.offer(events[i++][1]);
+      }
+      minHeap.poll();
+      ans++;
+      day++;
+      while (!minHeap.isEmpty() && minHeap.peek() < day) {
+        minHeap.poll();
+      }
+    }
+
+    return ans;
+  }
+}
+
