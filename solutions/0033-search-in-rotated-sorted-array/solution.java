798d7374f4619584d927d36163e5deeba59e612d
Agasthya R Kumar <147073909+agasthyarkumar@users.noreply.github.com>
2025-07-09 11:05:09 +0000

    [LeetCode | 06 Sep 2026 12:27:48 PM IST] Runtime - 0 ms (100.00%), Memory - 42 MB (99.98%)

--- solutions/0033-search-in-rotated-sorted-array/solution.java
+++ solutions/0033-search-in-rotated-sorted-array/solution.java
@@ -7,12 +7,12 @@ class Solution {
       final int m = (l + r) / 2;
       if (nums[m] == target)
         return m;
-      if (nums[l] <= nums[m]) { 
+      if (nums[l] <= nums[m]) { // nums[l..m] are sorted.
         if (nums[l] <= target && target < nums[m])
           r = m - 1;
         else
           l = m + 1;
-      } else { 
+      } else { // nums[m..n - 1] are sorted.
         if (nums[m] < target && target <= nums[r])
           l = m + 1;
         else
