1b839d41f10ec5212b009516ae096b481daa6c27
Agasthya R Kumar <147073909+agasthyarkumar@users.noreply.github.com>
2025-08-05 07:08:27 +0000

    [LeetCode] Runtime - 18 ms (75.26%), Memory - 17.9 MB (100.00%)

---
+++ solutions/3790-fruits-into-baskets-ii/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        n = len(fruits)
+        used = [False] * n
+        unplaced = 1
+        for i in range(n):
+            placed = False
+            for j in range(n):
+               if not used[j] and baskets[j] >= fruits[i]:
+                    used[j] = True
+                    placed = True
+                    break
+            if not placed:
+                unplaced += 1
+        return unplaced-1
