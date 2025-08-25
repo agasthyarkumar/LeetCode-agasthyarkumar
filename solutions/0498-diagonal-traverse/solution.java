c8c4a314baaa2532af6eb909bb2de2eaedf24ce7
Agasthya R Kumar <147073909+agasthyarkumar@users.noreply.github.com>
2025-08-25 07:31:00 +0000

    [LeetCode] Runtime - 2 ms (98.72%), Memory - 47.2 MB (98.90%)

---
+++ solutions/0498-diagonal-traverse/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+       if (mat == null || mat.length == 0) {
+            return new int[0];
+        }
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] result = new int[m * n];
+        int row = 0, col = 0;
+        for (int i = 0; i < result.length; i++) {
+            result[i] = mat[row][col];
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1) {
+                    row++;
+                } else if (row == 0) {
+                    col++;
+                } else {
+                    row--;
+                    col++;
+                }
+            } else {
+                if (row == m - 1) {
+                    col++;
+                } else if (col == 0) {
+                    row++;
+                } else {
+                    row++;
+                    col--;
+                }
+            }
+        }
+        return result;
+    }
+}
