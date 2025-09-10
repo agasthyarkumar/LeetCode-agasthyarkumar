375d190f352512ee15290ea160740a0972f14445
Agasthya R Kumar <147073909+agasthyarkumar@users.noreply.github.com>
2025-09-10 10:57:27 +0000

    [LeetCode | 06 Sep 2026 12:27:48 PM IST] Runtime - 1 ms (95.43%), Memory - 42.1 MB (99.98%)

--- solutions/0118-pascals-triangle/solution.java
+++ solutions/0118-pascals-triangle/solution.java
@@ -1,18 +1,17 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> pascal = new ArrayList<>();
-        pascal.add(Arrays.asList(1));
+  public List<List<Integer>> generate(int numRows) {
+    List<List<Integer>> ans = new ArrayList<>();
 
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>();
-            List<Integer> prev = pascal.get(i - 1);
-            row.add(1);
-            for (int j = 1; j < i; j++) {
-                row.add(prev.get(j - 1) + prev.get(j));
-            }
-            row.add(1);
-            pascal.add(row);
-        }
-        return pascal;
+    for (int i = 0; i < numRows; ++i) {
+      Integer[] temp = new Integer[i + 1];
+      Arrays.fill(temp, 1);
+      ans.add(Arrays.asList(temp));
     }
+
+    for (int i = 2; i < numRows; ++i)
+      for (int j = 1; j < ans.get(i).size() - 1; ++j)
+        ans.get(i).set(j, ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
+
+    return ans;
+  }
 }
